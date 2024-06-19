11f04a6a8105e853079cef22aceeb2c25a3c232c
cosimazink <cosima_hiromi.zink@smail.th-koeln.de>
2024-06-19 13:12:53 +0200

    first import

---
+++ src/main/kotlin/de/thk/gm/fddw/proxyparcelbox/ProxyparcelboxCosimaZinkApplication.kt
@@ -0,0 +1,11 @@
+package de.thk.gm.fddw.proxyparcelbox
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProxyparcelboxCosimaZinkApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProxyparcelboxCosimaZinkApplication>(*args)
+}
